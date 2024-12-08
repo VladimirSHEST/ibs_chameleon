e4d79c67cf194ca46e4f861cea3370e0c492d31e
Vladimir <162320414+VladimirSHEST@users.noreply.github.com>
2024-12-09 00:05:45 +0400

    Initial commit

---
+++ src/test/java/com/ibs/steps/TestSandBoxSteps.java
@@ -0,0 +1,18 @@
+package com.ibs.steps;
+
+import io.cucumber.java.ru.*;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.*;
+import java.util.concurrent.TimeUnit;
+
+public class MyStepdefs {
+    public MyStepdefs() {
+        Дано("^открыть страницу \"([^\"]*)\"$", (String arg0) -> {
+        });
+    }
+}
